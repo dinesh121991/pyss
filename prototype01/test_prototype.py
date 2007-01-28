3c30237c0594ddacae3cd6ce6451c112bc5fc27b
orip <orip@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-01-28 21:45:46 +0000

    first batch of tests (do they work?)

---
+++ prototype01/test_prototype.py
@@ -0,0 +1,14 @@
+import unittest
+import simulator_prototype_01 as prototype
+
+class test_cluster(unittest.TestCase):
+    NUM_PROCESSORS = 700
+    def setUp(self):
+        self.cluster = prototype.Cluster( num_processors = self.NUM_PROCESSORS )
+
+    def test_initial_idle_processors(self):
+        self.assertEqual(self.NUM_PROCESSORS, self.cluster.idle_processors)
+
+    def test_bla(self):
+        self.cluster.run_job(job = prototype.Job())
+        self.assertEqual
