96d34821749fd569e9e0068763cb643db82ec4be
orip <orip@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-01-28 20:46:49 +0000

    more skeleton, named the components

--- prototype01/simulator_prototype_01.py
+++ prototype01/simulator_prototype_01.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python
+
+class Scheduler(object):
+    pass
+
+class Workload(object):
+    pass
+
+class Cluster(object):
+    pass
+
+class Statistics(object):
+    pass
+
+if __name__ == '__main__':
+    pass
