3280336baa21c38a96a2cdf30f5d205d267e29d8
orip <orip@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-02-03 15:48:20 +0000

    Added test_next_event_{one_job,many_jobs}

--- prototype01/test_prototype.py
+++ prototype01/test_prototype.py
@@ -5,8 +5,15 @@ import prototype
 class test_EventQueue(TestCase):
     def setUp(self):
         self.queue = prototype.EventQueue()
+        self.event = prototype.JobEvent(timestamp=0, job_id=0)
+        self.events = [
+                prototype.JobEvent(timestamp=0, job_id=i)
+                for i in xrange(10)
+            ]
+
     def tearDown(self):
         del self.queue
+        del self.event
 
     def test_empty_initialization(self):
         self.assertEqual( 0, len(list(self.queue._events)) )
@@ -15,18 +22,26 @@ class test_EventQueue(TestCase):
         self.assertEqual( 0, len(list(self.queue._events)) )
 
     def test_add_event_sanity(self):
-        self.queue.add_event( prototype.JobEvent(0, 0) )
+        self.queue.add_event( self.event )
 
     def test_add_event_single_event(self):
-        event = prototype.JobEvent(timestamp=0, job_id=0)
-        self.queue.add_event(event)
-        self.assertEqual( [event], self.queue._events )
+        self.queue.add_event(self.event)
+        self.assertEqual( [self.event], self.queue._events )
 
     def test_add_event_simple(self):
-        events = [prototype.JobEvent(timestamp=0, job_id=i) for i in xrange(10)]
-        for event in events:
+        for event in self.events:
             self.queue.add_event(event)
-        self.assertEqual( events, list(self.queue._events) )
+        self.assertEqual( self.events, list(self.queue._events) )
+
+    def test_next_event_one_job(self):
+        self.queue.add_event( self.event )
+        assert self.queue.next_event is self.event
+
+    def test_next_event_many_jobs(self):
+        for event in self.events:
+            self.queue.add_event(event)
+        for event in self.events:
+            assert self.queue.next_event is event
 
 if __name__ == "__main__":
     try:
