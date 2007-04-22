3ac94b3311b6222a8ee2e311c0e04969d01f2361
oripel <oripel@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-04-22 23:57:25 +0000

    Main executable for running the simulator

---
+++ prototype01/run_prototype.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python2.4
+import sys
+import time
+
+from event_queue import EventQueue
+from prototype import Simulator, Machine, StupidScheduler
+import workload_parser
+
+def main():
+    print "Reading input from stdin..."
+
+    job_inputs = workload_parser.parse_lines( sys.stdin )
+    event_queue = EventQueue()
+    machine = Machine(num_processors=1000, event_queue=event_queue)
+    scheduler = StupidScheduler(event_queue)
+
+    simulator = Simulator(
+        job_inputs,
+        event_queue = event_queue,
+        machine = machine,
+        scheduler = scheduler,
+    )
+
+    simulator.run()
+
+if __name__ == "__main__":
+    main()
