8a2f742bd41dcc6384382a73c6db5d41b7624372
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-04-30 11:59:08 +0000

    the code of the easy backfill is now in the same code file of the maui

--- scheduler/sim3.py
+++ scheduler/sim3.py
@@ -6,6 +6,104 @@ from sim2 import *
 import sys
 
 
+
+
+class EasyBackfillScheduler(Scheduler):
+    
+    def __init__(self, total_nodes = 100):
+        self.cpu_snapshot = CpuSnapshot(total_nodes)
+        self.waiting_list_of_unscheduled_jobs = []
+
+        
+    def handleArrivalOfJobEvent(self, just_arrived_job, time):
+         self.waiting_list_of_unscheduled_jobs.append(just_arrived_job)
+         return self._schedule_jobs(time)  
+             
+
+    def handleTerminationOfJobEvent(self, job, time):
+        """ this handler deletes the tail of job.
+        It then reschedules the remaining jobs and returns a collection of new termination events
+        (using the dictionary data structure) """
+        self.cpu_snapshot.delTailofJobFromCpuSlices(job)
+        return self._schedule_jobs(time)
+
+
+    def _schedule_jobs(self, current_time):
+        newEvents = Events()
+                             
+        if len(self.waiting_list_of_unscheduled_jobs) == 0:
+            return newEvents # waiting list is empty
+        self._schedule_the_head_of_the_waiting_list(current_time, newEvents)
+        self._backfill_the_tail_of_the_waiting_list(current_time, newEvents)
+        return newEvents
+
+
+    
+
+    def _schedule_the_head_of_the_waiting_list(self, time, newEvents):
+        while len(self.waiting_list_of_unscheduled_jobs) > 0: 
+            first_job = self.waiting_list_of_unscheduled_jobs[0]
+            start_time_of_first_job = self.cpu_snapshot.jobEarliestAssignment(first_job, time)
+            if start_time_of_first_job == time:
+                self.waiting_list_of_unscheduled_jobs.remove(first_job)
+                self.cpu_snapshot.assignJob(first_job, time)
+                termination_time = time + first_job.actual_duration
+                newEvents.add_job_termination_event(termination_time, first_job) 
+            else:
+                break
+
+    def _backfill_the_tail_of_the_waiting_list(self, time, newEvents):
+        if len(self.waiting_list_of_unscheduled_jobs) > 1:
+            first_job = self.waiting_list_of_unscheduled_jobs[0]
+            for next_job in self.waiting_list_of_unscheduled_jobs[1:] : 
+                if self.canBeBackfilled(first_job, next_job, time):
+                    self.waiting_list_of_unscheduled_jobs.remove(next_job)
+                    time = self.cpu_snapshot.jobEarliestAssignment(next_job, time)
+                    self.cpu_snapshot.assignJob(next_job, time)
+                    termination_time = time + next_job.actual_duration
+                    newEvents.add_job_termination_event(termination_time, next_job)                    
+ 
+    
+
+    def canBeBackfilled(self, first_job, second_job, time):
+        print "... Let's check if the job can be backfilled"
+        
+        start_time_of_second_job = self.cpu_snapshot.jobEarliestAssignment(second_job, time)
+        print "start time of the 2nd job: ", start_time_of_second_job, second_job.id
+
+        if start_time_of_second_job > time:
+            return False
+    
+        shadow_time = self.cpu_snapshot.jobEarliestAssignment(first_job, time)
+        print "shadow time, the reserved start time of the first job: ", shadow_time, first_job.id
+        
+        # TODO: shouldn't this method not change the state?
+        self.cpu_snapshot.assignJob(second_job, time)
+        start_time_of_1st_if_2nd_job_assigned = self.cpu_snapshot.jobEarliestAssignment(first_job, time)
+        print "start time of the 1st job after assigning the 2nd: ",  start_time_of_1st_if_2nd_job_assigned
+        
+        self.cpu_snapshot.delJobFromCpuSlices(second_job)
+       
+        if start_time_of_1st_if_2nd_job_assigned > shadow_time:
+            print "reserved_start_time_of_first_job", shadow_time
+            print "strat_time_of_1st_if_2nd_job_assigned", start_time_of_1st_if_2nd_job_assigned
+            return False 
+                #this means that assigning the second job at current time postphones the
+                #first job in the waiting list, and so the second job cannot be back filled 
+        else:
+            return True 
+      
+
+    def handleEndOfSimulationEvent(self, current_time):
+         if current_time == sys.maxint:
+            # otherewise, it might be the case that the simulation stoped
+            # before some jobs were scheduled properly 
+            self.cpu_snapshot.CpuSlicesTestFeasibility()
+
+
+
+
+
 # a first toy version for the maui 
 
 class MauiScheduler(EasyBackfillScheduler):
@@ -21,6 +119,7 @@ class MauiScheduler(EasyBackfillScheduler):
         else:
             return  1
         
+
     def backfilling_compare(self, job_a, job_b):
         weight_a = 0 * job_a.admin_QoS + 1000 * job_a.user_QoS
         weight_b = 0 * job_b.admin_QoS + 1000 * job_b.user_QoS
