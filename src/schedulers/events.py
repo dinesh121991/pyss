a6c71c823712e13f04eb2ea41e9f0196e9cc84e3
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-06-01 13:17:52 +0000

    collection is now private

--- src/schedulers/events.py
+++ src/schedulers/events.py
@@ -7,32 +7,32 @@ from base.prototype import JobSubmissionEvent, JobTerminationEvent
 class Events:
     
     def __init__(self):
-        self.collection = {}
+        self._collection = {}
 
     def _addEvent(self, event): 
-         if self.collection.has_key(event.timestamp):
-             self.collection[event.timestamp].insert(0, event)
+         if self._collection.has_key(event.timestamp):
+             self._collection[event.timestamp].insert(0, event)
          else:
-             self.collection[event.timestamp] = []
-             self.collection[event.timestamp].insert(0, event)
+             self._collection[event.timestamp] = []
+             self._collection[event.timestamp].insert(0, event)
 
     def add_job_submission_event(self, timestamp, job): # adds a single submission event to the collection
         self._addEvent(JobSubmissionEvent(timestamp, job))
 
     @property
     def _min_event_time(self):
-        return min(self.collection.keys())
+        return min(self._collection.keys())
 
     @property
     def is_empty(self):
-        return len(self.collection) == 0
+        return len(self._collection) == 0
 
     def pop_min_event(self):
         assert not self.is_empty
-        assert len(self.collection[self._min_event_time]) > 0
-        result = self.collection[self._min_event_time].pop()
-        if len(self.collection[self._min_event_time]) == 0:
-            del self.collection[self._min_event_time]
+        assert len(self._collection[self._min_event_time]) > 0
+        result = self._collection[self._min_event_time].pop()
+        if len(self._collection[self._min_event_time]) == 0:
+            del self._collection[self._min_event_time]
         return result
         
     def add_job_termination_event(self, timestamp, job):
@@ -41,15 +41,15 @@ class Events:
         self._addEvent(JobTerminationEvent(timestamp, job))
 
     def addEvents(self, new_events): # combines a new collection of events with the self collection        
-         for timestamp, new_list_of_events_at_this_time in new_events.collection.iteritems():
+         for timestamp, new_list_of_events_at_this_time in new_events._collection.iteritems():
              for new_event in new_list_of_events_at_this_time:         
                  self._addEvent(new_event)                     
 
     def printEvents(self): # SHOULD IT BE __STR__????
-        times = self.collection.keys()
+        times = self._collection.keys()
         times.sort()
         for t in times:
-            for event in self.collection[t]: 
+            for event in self._collection[t]: 
                 print event 
         print
          
