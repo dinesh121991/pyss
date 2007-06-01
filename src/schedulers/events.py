733edd8e7ac2551418b8a1b8e9bd42b3ef2351e8
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-06-01 13:40:50 +0000

    add_event is now public

--- src/schedulers/events.py
+++ src/schedulers/events.py
@@ -10,11 +10,11 @@ class Events:
         from base.simple_heap import Heap
         self._events_heap = Heap()
 
-    def _addEvent(self, event): 
+    def add_event(self, event): 
         self._events_heap.push( (event.timestamp, event) )
 
     def add_job_submission_event(self, timestamp, job): # adds a single submission event to the collection
-        self._addEvent(JobSubmissionEvent(timestamp, job))
+        self.add_event(JobSubmissionEvent(timestamp, job))
 
     @property
     def is_empty(self):
@@ -28,8 +28,8 @@ class Events:
     def add_job_termination_event(self, timestamp, job):
         # makes sure that there will be a single termination event for this job
         # assert timestamp >= 0
-        self._addEvent(JobTerminationEvent(timestamp, job))
+        self.add_event(JobTerminationEvent(timestamp, job))
 
     def addEvents(self, new_events): # combines a new collection of events with the self collection        
         for (timestamp, new_event) in new_events._events_heap:
-            self._addEvent(new_event)                     
+            self.add_event(new_event)                     
