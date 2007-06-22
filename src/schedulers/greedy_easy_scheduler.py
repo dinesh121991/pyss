7ae907c1711a7a0683004cfa2e066ab1f616ee56
oripel <oripel@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-06-22 20:35:36 +0000

    Using sort key functions instead of comparison functions

--- src/schedulers/greedy_easy_scheduler.py
+++ src/schedulers/greedy_easy_scheduler.py
@@ -3,13 +3,13 @@ from base.prototype import JobStartEvent
 
 from easy_scheduler import EasyBackfillScheduler
 
-default_compare_functions = (
-    lambda job_a, job_b : cmp(job_b.submit_time, job_a.submit_time),
-    lambda job_a, job_b : cmp(job_a.submit_time, job_b.submit_time),
-    lambda job_a, job_b : cmp(job_a.num_processors, job_b.num_processors),
-    lambda job_a, job_b : cmp(job_a.estimated_run_time, job_b.estimated_run_time),
+default_sort_key_functions = (
+    lambda job : -job.submit_time, # sort by reverse submission time
+    lambda job : job.submit_time,
+    lambda job : job.num_processors,
+    lambda job : job.estimated_run_time,
     # TODO: why don't we use this one?
-    #lambda job_a, job_b : cmp(job_a.num_processors * job_a.estimated_run_time, job_b.num_processors * job_b.estimated_run_time),
+    #lambda job : job.num_processors * job.estimated_run_time,
 )
 
 def basic_score_function(list_of_jobs):
@@ -19,13 +19,13 @@ def basic_score_function(list_of_jobs):
     return val
 
 class  GreedyEasyBackFillScheduler(EasyBackfillScheduler):
-    def __init__(self, num_processors, compare_functions=None, score_function=None):
+    def __init__(self, num_processors, sort_key_functions=None, score_function=None):
         super(GreedyEasyBackFillScheduler, self).__init__(num_processors)
 
-        if compare_functions is None:
-            self.compare_functions = default_compare_functions
+        if sort_key_functions is None:
+            self.sort_key_functions = default_sort_key_functions
         else:
-            self.compare_functions = compare_functions
+            self.sort_key_functions = sort_key_functions
 
         if score_function is None:
             self.score_function = basic_score_function
@@ -33,11 +33,11 @@ class  GreedyEasyBackFillScheduler(EasyBackfillScheduler):
             self.score_function = score_function
 
     def _schedule_jobs(self, current_time):
-        self.unscheduled_jobs.sort(self.submit_time_compare)
+        self.unscheduled_jobs.sort(key = self._submit_job_sort_key)
 
         result = super(GreedyEasyBackFillScheduler, self)._schedule_jobs(current_time)
 
-        self.unscheduled_jobs.sort(self.submit_time_compare)
+        self.unscheduled_jobs.sort(key = self._submit_job_sort_key)
 
         return result
 
@@ -60,12 +60,12 @@ class  GreedyEasyBackFillScheduler(EasyBackfillScheduler):
 
         cpu_snapshot_copy.assignJobEarliest(first_job, current_time)
 
-        max_score_compare_func = self.compare_functions[0]
+        max_score_sort_key_func = self.sort_key_functions[0]
         max_score = 0.0
-        for compare_func in self.compare_functions:
+        for sort_key_func in self.sort_key_functions:
             tmp_cpu_snapshot = cpu_snapshot_copy.clone()
             tentative_list_of_jobs = []
-            self.unscheduled_jobs.sort(compare_func)
+            self.unscheduled_jobs.sort(key=sort_key_func)
             for job in self.unscheduled_jobs:
                 if tmp_cpu_snapshot.canJobStartNow(job, current_time):
                     tmp_cpu_snapshot.assignJob(job, current_time)
@@ -74,13 +74,13 @@ class  GreedyEasyBackFillScheduler(EasyBackfillScheduler):
             score = self.score_function(tentative_list_of_jobs)
             if max_score < score:
                 max_score = score
-                max_score_compare_func = compare_func
+                max_score_sort_key_func = sort_key_func
 
-        self.unscheduled_jobs.sort(max_score_compare_func)
+        self.unscheduled_jobs.sort(key=max_score_sort_key_func)
         self.unscheduled_jobs.append(first_job)
 
-    def submit_time_compare(self, job_a, job_b):
-        return cmp(job_a.submit_time, job_b.submit_time)
+    def _submit_job_sort_key(self, job):
+        return job.submit_time
 
     def print_waiting_list(self):
         for job in self.unscheduled_jobs:
