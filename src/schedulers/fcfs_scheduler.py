96a2efb31fa7df5729c949294bc6069fa33839bb
oripel <oripel@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-06-22 22:06:06 +0000

    FcfsScheduler._schedule_jobs now returns jobs instead of events, like the EASY family

--- src/schedulers/fcfs_scheduler.py
+++ src/schedulers/fcfs_scheduler.py
@@ -11,22 +11,27 @@ class FcfsScheduler(Scheduler):
     def handleSubmissionOfJobEvent(self, job, current_time):
         self.cpu_snapshot.archive_old_slices(current_time)
         self.waiting_queue_of_jobs.append(job)
-        return self._schedule_jobs(current_time)
+        return [
+            JobStartEvent(current_time, job)
+            for job in self._schedule_jobs(current_time)
+        ]
 
     def handleTerminationOfJobEvent(self, job, current_time):
         self.cpu_snapshot.archive_old_slices(current_time)
         self.cpu_snapshot.delTailofJobFromCpuSlices(job)
-        return self._schedule_jobs(current_time)
-
+        return [
+            JobStartEvent(current_time, job)
+            for job in self._schedule_jobs(current_time)
+        ]
 
     def _schedule_jobs(self, current_time):
-        newEvents = []
+        result = []
         while len(self.waiting_queue_of_jobs) > 0:
             job = self.waiting_queue_of_jobs[0]
             if self.cpu_snapshot.free_processors_available_at(current_time) >= job.num_required_processors:
                 self.waiting_queue_of_jobs.pop(0)
                 self.cpu_snapshot.assignJob(job, current_time)
-                newEvents.append( JobStartEvent(current_time, job) )
+                result.append(job)
             else:
                 break
-        return newEvents
+        return result
