69819bb610b0651142b3cbda9ac05ad84cdfab76
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-07-09 15:24:05 +0000

    working more on the tests

--- src/schedulers/tests.py
+++ src/schedulers/tests.py
@@ -221,7 +221,7 @@ class test_Simulator(unittest.TestCase):
                                  "i="+str(i)+" "+str(job) + str(job.finish_time))
 
     def test_easyPlusPlusBackfill(self):
-        for i in range(14):
+        for i in range(15):
             scheduler = EasyPlusPlusScheduler(NUM_PROCESSORS)
             simulator = run_simulator(scheduler=scheduler, num_processors=NUM_PROCESSORS, \
                                       input_file = INPUT_FILE_DIR + "/plus_plus_easy." + str(i))
