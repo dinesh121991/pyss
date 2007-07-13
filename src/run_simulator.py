98945300293fa5a49dd29ed90b1dccc547969ab2
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-07-13 10:40:35 +0000

    initial attempt at a real working executable

--- src/run_simulator.py
+++ src/run_simulator.py
@@ -1,33 +1,32 @@
 #! /usr/bin/env python2.4
+
 import sys
-import time
-
-from base.event_queue import EventQueue
-from base.prototype import Machine, StupidScheduler, _job_inputs_to_jobs
-from base import workload_parser
-
-from schedulers.simulator import Simulator
-
-from fcfs_scheduler import FcfsScheduler
-
 if __debug__:
     import warnings
     warnings.warn("Running in debug mode, this will be slow... try 'python2.4 -O %s'" % sys.argv[0])
 
+from base.workload_parser import parse_lines
+from base.prototype import _job_inputs_to_jobs
+from schedulers.simulator import run_simulator
+
 def main():
-    print "Reading input from stdin..."
+    input_filename = sys.argv[1]
+    print "Reading %r..." % input_filename
 
-    event_queue = EventQueue()
-    scheduler = FcfsScheduler(event_queue=event_queue, num_processors=num_processors)
+    NUM_PROCESSORS = 100 # TODO: take from input instead of magic number
 
-    simulator = Simulator(
-        jobs = _job_inputs_to_jobs(workload_parser.parse_lines(sys.stdin)),
-        event_queue = event_queue,
-        num_processors = 1000,
-        scheduler = scheduler,
-    )
+    input_file = open(input_filename)
+    try:
+        run_simulator(
+                num_processors = NUM_PROCESSORS,
+                jobs = _job_inputs_to_jobs(parse_lines(input_file)),
+                scheduler = MauiScheduler(NUM_PROCESSORS)
+            )
 
-    simulator.run()
+        print "done."
+
+    finally:
+        input_file.close()
 
 if __name__ == "__main__":
     main()
---
+++ src/run_simulator.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+PYTHONPATH=.:$PYTHONPATH python2.4 run_simulator.py $*
