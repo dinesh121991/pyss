88e4f52693eb652279559fbdf2ee0164e73f4c8b
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-09-04 16:04:10 +0000

    nothing special

--- src/schedulers/probabilistic_easy_scheduler.py
+++ src/schedulers/probabilistic_easy_scheduler.py
@@ -8,7 +8,7 @@ class Distribution(object):
     def __init__(self, job):
         self.bins    = {}
         self.bins[1] = 1 # adding the first entry to the main data structure of the distribution 
-        self.number_of_jobs_added = 1
+        self.number_of_jobs_added = 1.0
 
         if job is not None: # we init the distribution to be uniform w.r.t. to the user estimation 
             self.touch(job.user_estimated_run_time)
@@ -18,8 +18,8 @@ class Distribution(object):
         rounded_up_time = pow(2, int(log(max(2 * time - 1, 1), 2)))
        	while rounded_up_time > 1: 
             if not self.bins.has_key(rounded_up_time) or self.bins[rounded_up_time] == 0:  
-                self.bins[rounded_up_time] = 1  
-		self.number_of_jobs_added += 1
+                self.bins[rounded_up_time] = 1.0  
+		self.number_of_jobs_added += 1.0
             rounded_up_time = rounded_up_time / 2
             
 
@@ -27,7 +27,7 @@ class Distribution(object):
         rounded_up_time = pow(2, int(log(max(2 * time - 1, 1) , 2))) 
        	while rounded_up_time > 1: 
             if not self.bins.has_key(rounded_up_time):  
-                self.bins[rounded_up_time] = 0  
+                self.bins[rounded_up_time] = 0.0  
             rounded_up_time = rounded_up_time / 2
 
             
@@ -35,14 +35,14 @@ class Distribution(object):
         assert job.actual_run_time > 0
         
         rounded_up_run_time = pow(2, int(log(max(2 * job.actual_run_time - 1, 1), 2)))
-        self.number_of_jobs_added += 1
+        self.number_of_jobs_added += 1.0
         
         if self.bins.has_key(rounded_up_run_time):
-            self.bins[rounded_up_run_time] += 1 # incrementing the numbers of terminated jobs encountered so far
+            self.bins[rounded_up_run_time] += 1.0 # incrementing the numbers of terminated jobs encountered so far
         else: 
-            self.bins[rounded_up_run_time]  = 1   # we add a new entry initialized to 1
+            self.bins[rounded_up_run_time]  = 1.0   # we add a new entry initialized to 1
             self.emptytouch(rounded_up_run_time) # and "add" empty entries in between 
-        
+
             
         
 class  ProbabilisticEasyScheduler(Scheduler):
