ac15641ec921dcf7a0f3db45754392777681748d
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-09-25 14:20:24 +0000

    mega-commit: Lots of changes in OrigProbabilisticEasyScheduler

--- src/schedulers/orig_probabilistic_easy_scheduler.py
+++ src/schedulers/orig_probabilistic_easy_scheduler.py
@@ -4,11 +4,14 @@ from math import log
 
     
 class Distribution(object):
-    def __init__(self, job):
-        self.bins    = {}
-        self.bins[1] = 1 # adding the first entry to the main data structure of the distribution 
+    def __init__(self, job, window_size = 150):
+        self.bins     = {}
+        self.bins[1]  = 1 # adding the first entry to the main data structure of the distribution 
         self.number_of_jobs_added = 1
-
+        
+        self.window_size = window_size # the distribution contains information about at most window_size (recently terminated) jobs    
+        self.jobs     = []
+        
         if job is not None: # we init the distribution to be uniform w.r.t. to the user estimation 
             self.touch(job.user_estimated_run_time)
             
@@ -30,20 +33,34 @@ class Distribution(object):
             
     def add_job(self, job): #to be called when a termination event has occured
         assert job.actual_run_time > 0     
-        rounded_up_run_time = pow(2, int(log(2 * job.actual_run_time, 2)))
-        self.touch(rounded_up_run_time, rounded=True)   
+        rounded_up_run_time = pow(2, int(log(2 * job.actual_run_time-1, 2)))
+        self.touch(rounded_up_run_time, True)   
+
         self.number_of_jobs_added += 1
         self.bins[rounded_up_run_time] += 1 # incrementing the numbers of terminated jobs encountered so far
+        
+        self.jobs.append(job)
+        if len(self.jobs) > self.window_size:
+            self.del_job(self.jobs.pop(0))
+        
 
-            
+    def del_job(self, job):
+        rounded_up_run_time = pow(2, int(log(2 * job.actual_run_time-1, 2)))
+	assert self.number_of_jobs_added >= self.bins[rounded_up_run_time] > 0
+   
+        self.number_of_jobs_added -= 1
+        self.bins[rounded_up_run_time] -= 1
+
+
         
 class  OrigProbabilisticEasyScheduler(Scheduler):
     """ This algorithm implements a version of Feitelson and Nissimov, June 2007
     """
     
-    def __init__(self, num_processors, threshold = 0.05):
+    def __init__(self, num_processors, threshold = 0.05, window_size=150):
         super(OrigProbabilisticEasyScheduler, self).__init__(num_processors)
-        self.threshold = threshold
+        self.threshold    = threshold
+        self.window_size  = window_size # a parameter for the distribution 
         self.cpu_snapshot = CpuSnapshot(num_processors)
         
         self.user_distribution = {}
@@ -56,7 +73,7 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
         if  self.user_distribution.has_key(job.user_id):
             self.user_distribution[job.user_id].touch(job.user_estimated_run_time)
         else:
-            self.user_distribution[job.user_id] = Distribution(job)
+            self.user_distribution[job.user_id] = Distribution(job, self.window_size)
 
         self.cpu_snapshot.archive_old_slices(current_time)
         self.unscheduled_jobs.append(job)
@@ -107,16 +124,14 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
 
         result    = []  
         first_job = self.unscheduled_jobs[0]        
-        tail      = self.unscheduled_jobs[1:]
+        tail      = list_copy(self.unscheduled_jobs[1:]) 
                 
         for job in tail:
             if self.can_be_probabilistically_backfilled(job, current_time):
-                # print "+++ job can be backfilled", job
                 self.unscheduled_jobs.remove(job)
                 self.currently_running_jobs.append(job)
                 self.cpu_snapshot.assignJob(job, current_time)
-                result.append(job)
-                
+                result.append(job)                
         return result
 
 
@@ -129,51 +144,31 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
 
         first_job = self.unscheduled_jobs[0]
 
-        rounded_up_time = pow(2, int(log(2 * job.user_estimated_run_time, 2)))
-        
+        rounded_up_time = pow(2, int(log(2 * job.user_estimated_run_time, 2)))       
         for tmp_job in self.currently_running_jobs:
-            self.user_distribution[tmp_job.user_id].touch(rounded_up_time, rounded=True)
+            self.user_distribution[tmp_job.user_id].touch(rounded_up_time, True)
       
         prediction  = 0.0
         max_bottle_neck = 0.0 
         bottle_neck = 0.0 
         t = 1
 
-        C = first_job.num_required_processors + job.num_required_processors
-
-        if C <= self.num_processors:
-            while t < 2 * job.user_estimated_run_time:
-                job_probability_to_end_at_t = self.probability_to_end_at(t, job)
-                max_bottle_neck = max(max_bottle_neck, self.bottle_neck(t, job, first_job, current_time, 1))
-                prediction += job_probability_to_end_at_t * max_bottle_neck
-                # print "t is:", t
-                t = t * 2 
+        while t < 2 * job.user_estimated_run_time:
+            job_probability_to_end_at_t = self.probability_to_end_at(t, job)
+            max_bottle_neck = max(max_bottle_neck, self.bottle_neck(t, job, first_job, current_time))
+            prediction += job_probability_to_end_at_t * max_bottle_neck
+            t = t * 2 
     
-            if prediction <= self.threshold:
-                # print "prediction:", prediction
-                return True
-        else: 
-            while t < 2 * job.user_estimated_run_time:
-                job_probability_to_end_at_t = self.probability_to_end_at(t, job)
-                bottle_neck = self.bottle_neck(t, job, first_job, current_time, 2)
-                prediction += job_probability_to_end_at_t * bottle_neck
-                t = t * 2 
-
-            if prediction >= 1 - self.threshold:
-                return True
-        
+        if prediction <= self.threshold:
+            return True
+         
         return False
         
 
-    def bottle_neck(self, time, second_job, first_job, current_time, flag):
-        result = 0.0
-        M = {}
+    def bottle_neck(self, time, second_job, first_job, current_time):
         C = first_job.num_required_processors + second_job.num_required_processors
         K = min(self.num_processors, C)
-        
-        # M[n,c] denotes the probablity that at time the first n jobs among those that
-        # are currently running have released at least c processors
-        # print ">>> in bottle neck, current time is:", current_time
+        M = {} # M[n,c] denotes the probability that the first n running jobs will release at least c processors at time
 
         num_of_currently_running_jobs = len(self.currently_running_jobs)
         
@@ -184,26 +179,26 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
             M[n, 0] = 1.0
 
         for n in xrange(1, num_of_currently_running_jobs+1):
-            job = self.currently_running_jobs[n-1] # this is the n'th job, recall that a list has a zero index   
-            Pn = self.probability_of_running_job_to_end_upto(time, current_time, job)
+            job_n = self.currently_running_jobs[n-1] # the n'th job: recall that a list has a zero index   
+            Pn = self.probability_of_running_job_to_end_upto(time, current_time, job_n)
             for c in xrange (1, K + 1):
-                if c >= job.num_required_processors:  
-                    M[n, c] = M[n-1, c] + (M[n-1, c - job.num_required_processors] - M[n-1, c]) * Pn
+                if c >= job_n.num_required_processors:  
+                    M[n, c] = M[n-1, c] + (M[n-1, c - job_n.num_required_processors] - M[n-1, c]) * Pn
                 else:
                     M[n, c] = M[n-1, c] + (1 - M[n-1, c]) * Pn
 
 
-
         last_row_index = num_of_currently_running_jobs
-        if   flag == 1:  
-                result = M[last_row_index, first_job.num_required_processors] - M[last_row_index, C]
-        else: # flag == 2  
-                result = 1 - M[last_row_index, first_job.num_required_processors]
+        if  C <= K:  
+            result = M[last_row_index, first_job.num_required_processors] - M[last_row_index, C]
+        else:   
+            result = M[last_row_index, first_job.num_required_processors]
 
         if   result < 0:
             result = 0
         elif result > 1:
             reuslt = 1
+            
         assert 0 <= result <= 1 
         return result 
 
@@ -234,22 +229,24 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
                 num_of_jobs_in_middle_bins += job_distribution.bins[key] 
 
             elif time + rounded_down_run_time > key/2 : 
-                num_of_jobs_in_middle_bins += float(job_distribution.bins[key] *(time+rounded_down_run_time-(key/2))) / (key/2) 
+                num_of_jobs_in_middle_bins += float(job_distribution.bins[key]*(time+rounded_down_run_time-(key/2))) / (key/2) 
 
           
         num_of_irrelevant_jobs = num_of_jobs_in_first_bins + num_of_jobs_in_last_bins
         num_of_relevant_jobs = job_distribution.number_of_jobs_added - num_of_irrelevant_jobs+1 # +1 avoiding devision by zero
 
+	assert 0 <= num_of_jobs_in_middle_bins <= num_of_relevant_jobs, str(num_of_jobs_in_middle_bins)+str(" ")+str(num_of_relevant_jobs)
+
         result = num_of_jobs_in_middle_bins / num_of_relevant_jobs
-    
-        assert 0 <= result <= 1
+
         return result 
 
 
     def probability_to_end_at(self, time, job):         
         job_distribution = self.user_distribution[job.user_id]
         assert job_distribution.bins.has_key(time) == True
-        
+	        
+
         num_of_jobs_in_last_bins = 0
         rounded_up_user_estimated_run_time = 2 * job.user_estimated_run_time - 1
 
@@ -259,9 +256,10 @@ class  OrigProbabilisticEasyScheduler(Scheduler):
  
         num_of_relevant_jobs = job_distribution.number_of_jobs_added - num_of_jobs_in_last_bins+1 # +1 avoiding devision by zero
 
-        result = float(job_distribution.bins[time]) / num_of_relevant_jobs
-    
-        assert 0 <= result <= 1
+	assert 0 <= job_distribution.bins[time] <= num_of_relevant_jobs,str(time)+str(" ")+ str(job_distribution.bins[time])+str(" ")+str(num_of_relevant_jobs)
+
+       	result = float(job_distribution.bins[time]) / num_of_relevant_jobs
+
         return result 
      
      
