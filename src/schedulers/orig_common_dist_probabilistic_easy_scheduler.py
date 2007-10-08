9f54a45cb343b9b0f8cc907490ee3d5d00e6703d
thedayafter2005 <thedayafter2005@cb791f73-4c29-0410-96e1-29bb47b5ff47>
2007-10-08 11:56:42 +0000

    renaming orig_single_dist_probabilistic_easy_scheduler.py

--- src/schedulers/orig_common_dist_probabilistic_easy_scheduler.py
+++ src/schedulers/orig_common_dist_probabilistic_easy_scheduler.py
@@ -1,7 +1,7 @@
 from orig_probabilistic_easy_scheduler import OrigProbabilisticEasyScheduler
 
     
-class  OrigSingleDistProbabilisticEasyScheduler(OrigProbabilisticEasyScheduler):
+class  OrigCommonDistProbabilisticEasyScheduler(OrigProbabilisticEasyScheduler):
     """ This algorithm implements a version of Feitelson and Nissimov, June 2007
         In this simplified version we have a single common distribution for all the jobs and users 
     """
@@ -9,4 +9,4 @@ class  OrigSingleDistProbabilisticEasyScheduler(OrigProbabilisticEasyScheduler):
         # TODO: Ugly patch - make all jobs have the same user
         # Warning - loses the original user_id, don't use the same jobs with other schedulers after
         job.user_id = "common_user"
-        return super(OrigSingleDistProbabilisticEasyScheduler, self).new_events_on_job_submission(job, current_time)
+        return super(OrigCommonDistProbabilisticEasyScheduler, self).new_events_on_job_submission(job, current_time)
